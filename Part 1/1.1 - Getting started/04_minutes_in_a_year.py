ea005eb9d47fa18d5ce0c6766f4766e765fa2ec7
Joeeeps <kodewingy@gmail.com>
2024-05-13 21:30:23 +0100

    adding parts 1.1,1.2,1.3,1.4

---
+++ Part 1/1.1 - Getting started/04_minutes_in_a_year.py
@@ -0,0 +1,2 @@
+# Write your solution here
+print(365*60*24)
---
+++ Part 1/1.4 - Arithmetic operations/08_students_in_groups.py
@@ -0,0 +1,6 @@
+# Write your solution here
+students = int(input("How many students on the course? "))
+group = int(input("Desired group size? "))
+formed = -students//group
+
+print(f"Number of groups formed: {-formed}")
