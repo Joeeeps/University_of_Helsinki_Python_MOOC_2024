5e568bfbed13883597f2270fc426abc958a2e218
Joeeeps <kodewingy@gmail.com>
2024-05-14 20:48:36 +0100

    fine

--- Part 1/1.5 - Conditional Statements/10_quadratic_equation.py
+++ Part 1/1.5 - Conditional Statements/10_quadratic_equation.py
@@ -1,7 +1,6 @@
 # Write your solution here
 # Let's take the square root of math-module in use
 from math import sqrt
-
 a = int(input("Value of a: "))
 b = int(input("Value of b: "))
 c = int(input("Value of c: "))
